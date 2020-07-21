e8743a23ccfb249eb5a3c1103f0db76b2e879098
Miklos Kiss <kissmiklosjr@gmail.com>
2020-07-21 22:22:44 +0200

    feat: add db component

---
+++ components/db.go
@@ -0,0 +1,21 @@
+package components
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func NewDB(connectionString string) *mongo.Client {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(connectionString))
+	if err != nil {
+		fmt.Println("Fatal")
+	}
+	return client
+}
--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/go-redis/redis/v8 v8.0.0-beta.6
 	github.com/gorilla/mux v1.7.4
 	github.com/streadway/amqp v1.0.0
+	go.mongodb.org/mongo-driver v1.3.5
 )
