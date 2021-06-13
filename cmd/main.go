4433f76bc487a07a5305f3f99dd5ab6a421e3b1e
Miklos Kiss <kissmiklosjr@gmail.com>
2021-06-14 00:18:53 +0200

    save

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,9 @@ import (
 )
 
 func main() {
+	http.HandleFunc("/headlines", function.GetHeadlines)
+	http.HandleFunc("/everything", function.GetEverything)
+	http.HandleFunc("/countries", function.GetCountries)
 	http.HandleFunc("/scrape-news-api", function.ScrapeNewsApi)
 	http.ListenAndServe(":8080", nil)
 }
